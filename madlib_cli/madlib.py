a0c8cc0eb08e4894d97a49ad5f2d90bfd08e265c
11mones <mones_leader@hotmail.com>
2023-05-13 16:33:14 +0000

    The game is done now

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,14 +1,17 @@
 import re
 def read_template (path) :
-    with open (path, "r") as f:
-        text = f.read()
-        
-    return text
+    try :
+        with open ("../" + path, "r") as f:
+            text = f.read()
+
+        return text
+    except FileNotFoundError:
+        return "Please check of the path of your text file"
 
 
-t = read_template("../assets/dark_and_stormy_night_template.txt")
 
-print(1111,read_template("../assets/dark_and_stormy_night_template.txt"))
+
+
 
 def find_between(s, first, last):
     try:
@@ -19,21 +22,78 @@ def find_between(s, first, last):
         return -1
     
 def parse_template (t) :
+    regex = "\{(.+?)\}"
+import re
+def read_template (path) :
+    try :
+        with open ("../" + path, "r") as f:
+            text = f.read()
+
+        return text
+    except  KeyError as err:
+        return err
+
+
+
+
+ 
+def find_between(s, first, last):
+    try:
+        regex = rf'{first}(.*?){last}'
+        parts  = (re.findall(regex, s))
+        return parts
+    except ValueError:
+        return -1
+    
+def parse_template (t) :
+    regex = "\{(.+?)\}"
     for x in t:
-        res1 = t.replace("{Adjective}" , "{}")
+        # res1 = t.replace(regex , "{}")
+        x = re.sub(regex, "{}", t )
 
     res2 = find_between(t,"{", "}")
     my_tuple= (*res2,) 
-    return res1 , my_tuple
+    return x , my_tuple
+
+def merge(blank , tuple) :
+
+    blank = blank.format(*tuple)
+    return blank
 
 
+if __name__=="__main__" : 
+    print("""
+    Welcome to madlib game <3"
+    ---------------------------
+    In this game you will be asked to enter some words and these words will be in a funny article.
+    """)
+          
+# reading = read_template("assets/dark_and_stormy_night_template.txt")
+# path = "assets/dark_and_stormy_night_template.txt"
+    t = read_template("assets/myText.txt")
+    countOfWords = len(find_between(t , "{" , "}"))
+    print("Now please Enter" ,countOfWords,"words")
+    wordsList = []
+    for i in range(countOfWords):
+        i = input()
+        wordsList.append(i)
+    t = read_template("assets/myText.txt")
+    path = "assets/dark_and_stormy_night_template.txt"
 
-str, x = parse_template(t) 
-print(str)
-print(x)
-print(parse_template(t))
+    str, x = parse_template(t)
+    print("""
+    Your article is generated ....
+        Here is it : 
+          """)
 
-# print(parse_template(t))
+
+    print (merge(str , wordsList))
+    completed_text = merge(str , wordsList)
+    f = open("comp.txt", "a")
+    f.write(completed_text)
+    f.close()
+
+
 
 
 
@@ -41,5 +101,6 @@ print(parse_template(t))
 
 
  
+
 
 
